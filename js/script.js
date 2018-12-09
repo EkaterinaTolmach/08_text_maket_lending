0c9be093a2fb9ce72b529cca21379030fda2b443
EkaterinaTolmach <turina_ekaterina@mail.ru>
2018-12-09 18:12:45 +0800

    #add Добавлен скрипт инициализирующий слайдер отзывов

--- js/script.js
+++ js/script.js
@@ -12,4 +12,18 @@ $(document).ready(function() {
             nextArrow: '<a href="#" class="outside-print__slider-next"></a>'
         });
     }
+    if($('.testimonials-slider')) {
+        $('.testimonials-slider').slick({
+            infinite: true,
+            centerMode: true,
+            // centerPadding: '25px',
+            slidesToShow: 1,
+            arrows: true,
+            cssEase: 'linear',
+            variableWidth: true,
+            appendArrows: $('.testimonials__slider-controls'),
+            prevArrow: '<a href="#" class="testimonials__slider-prev"></a>',
+            nextArrow: '<a href="#" class="testimonials__slider-next"></a>'
+        });
+    }
 });
